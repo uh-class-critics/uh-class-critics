625892122f547bb7e16ff89aa67dd82b005846bd
Zi Jun Huang <54689008+ZijunHuang-1@users.noreply.github.com>
2021-04-14 21:34:53 -1000

    Update ProfessorReview.jsx

--- app/imports/ui/pages/ProfessorReview.jsx
+++ app/imports/ui/pages/ProfessorReview.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Grid, Card } from 'semantic-ui-react';
+import { Grid, Input, Container } from 'semantic-ui-react';
 
-export default class DescriptionCardRow extends React.Component {
-    render() {
-        return (
-            <Grid.Row>
-                <Card>
-                    <Card.Content>
-                    
-                    </Card.Content>
-                </Card>
-            </Grid.Row>
-        );
-    }
+class ProfessorReview extends React.Component {
+  render() {
+    return (
+      <Container fluid centered className='form'>
+        <Grid centered verticalAlign='middle' className='enter'>
+          <Grid.column textAlign='center' width={0.5}>
+            <h>Professor Name:</h>
+          </Grid.column>
+          <Grid.column textAlign='center' width={1}>
+            <Input placeholder='' />
+          </Grid.column>
+        </Grid>
+      </Container>
+    );
+  }
 }
+
+export default ProfessorReview;
