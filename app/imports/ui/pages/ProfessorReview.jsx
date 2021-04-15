c54e1a87c98ac5ed28172a6b299c6f220ad480e0
Zi Jun Huang <54689008+ZijunHuang-1@users.noreply.github.com>
2021-04-14 21:35:05 -1000

    Revert "Update ProfessorReview.jsx"
    This reverts commit 625892122f547bb7e16ff89aa67dd82b005846bd.

--- app/imports/ui/pages/ProfessorReview.jsx
+++ app/imports/ui/pages/ProfessorReview.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
-import { Grid, Input, Container } from 'semantic-ui-react';
+import { Grid, Card } from 'semantic-ui-react';
 
-class ProfessorReview extends React.Component {
-  render() {
-    return (
-      <Container fluid centered className='form'>
-        <Grid centered verticalAlign='middle' className='enter'>
-          <Grid.column textAlign='center' width={0.5}>
-            <h>Professor Name:</h>
-          </Grid.column>
-          <Grid.column textAlign='center' width={1}>
-            <Input placeholder='' />
-          </Grid.column>
-        </Grid>
-      </Container>
-    );
-  }
+export default class DescriptionCardRow extends React.Component {
+    render() {
+        return (
+            <Grid.Row>
+                <Card>
+                    <Card.Content>
+                    
+                    </Card.Content>
+                </Card>
+            </Grid.Row>
+        );
+    }
 }
-
-export default ProfessorReview;
