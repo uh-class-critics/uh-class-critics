fe978829091dea3951bccb9e2bee8283aca34f85
Zi Jun Huang <54689008+ZijunHuang-1@users.noreply.github.com>
2021-04-14 22:20:50 -1000

    commit 2
    Implemented form that doesn't connect with database

--- app/imports/ui/pages/ProfessorReview.jsx
+++ app/imports/ui/pages/ProfessorReview.jsx
@@ -1,83 +1,45 @@
 import React from 'react';
-import { Grid, Loader, Header, Segment, Rating } from 'semantic-ui-react';
-import swal from 'sweetalert';
-import {
-  AutoForm,
-  ErrorsField,
-  HiddenField,
-  LongTextField,
-  SubmitField,
-  TextField,
-} from 'uniforms-semantic';
-import { Meteor } from 'meteor/meteor';
-import { withTracker } from 'meteor/react-meteor-data';
-import PropTypes from 'prop-types';
-import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import { Grid, Input, Container, Header, TextArea,Form } from 'semantic-ui-react';
 
-import { Contacts } from '../../api/contact/Contacts';
-
-const bridge = new SimpleSchema2Bridge(Contacts.schema);
-
-/** Renders the Page for editing a single document. */
-class EditContact extends React.Component {
-
-  // On successful submit, insert the data.
-  submit(data) {
-    const { firstName, lastName, address, image, description, _id } = data;
-    Contacts.collection.update(_id, { $set: { firstName, lastName, address, image, description } }, (error) => (error ?
-      swal('Error', error.message, 'error') :
-      swal('Success', 'Item updated successfully', 'success')));
-  }
-
-  // If the subscription(s) have been received, render the page, otherwise show a loading icon.
+class ProfessorReview extends React.Component {
   render() {
-    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
-  }
-
-  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
-  renderPage() {
     return (
-      <Grid container centered>
-        <Grid.Column>
-          <Header as="h2" textAlign="center">Edit Stuff</Header>
-          <AutoForm schema={bridge} onSubmit={data => this.submit(data)} model={this.props.doc}>
-            <Segment>
-              <TextField name='firstName'/>
-              <TextField name='lastName'/>
-              <Rating icon='star' defaultRating={3} maxRating={4} />
-              <TextField name='address'/>
-              <TextField name='image'/>
-              <LongTextField name='description'/>
-              <SubmitField value='Submit'/>
-              <ErrorsField/>
-              <HiddenField name='owner' />
-            </Segment>
-          </AutoForm>
-        </Grid.Column>
-      </Grid>
-    );
+        <Container fluid>
+          <Container fluid centered className='form'>
+            <Grid centered verticalAlign='middle' className='enter'>
+
+              <Grid.Column>
+                <Grid.Column textAlign='center' width={0.5}>
+                  <h>Professor Name:</h>
+                </Grid.Column>
+                <Grid.Column textAlign='center' width={1}>
+                  <Input placeholder=''/>
+                </Grid.Column>
+
+                <Grid.Column textAlign='center' width={0.5}>
+                  <h>Rate Professor 1-5:</h>
+                </Grid.Column>
+                <Grid.Column textAlign='center' width={1}>
+                  <Input placeholder=''/>
+                </Grid.Column>
+
+                <Grid.Column textAlign='center' width={0.5}>
+                  <h>Describe the Professor:</h>
+                </Grid.Column>
+                <Grid.Column textAlign='center' width={1}>
+                  <Form>
+                    <TextArea placeholder=''/>
+                  </Form>
+                </Grid.Column>
+
+              </Grid.Column>
+            </Grid>
+          </Container>
+
+        </Container>
+    )
+        ;
   }
 }
 
-// Require the presence of a Stuff document in the props object. Uniforms adds 'model' to the props, which we use.
-EditContact.propTypes = {
-  doc: PropTypes.object,
-  model: PropTypes.object,
-  ready: PropTypes.bool.isRequired,
-};
-
-// withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-export default withTracker(({ match }) => {
-  // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
-  const documentId = match.params._id;
-  // Get access to Stuff documents.
-  const subscription = Meteor.subscribe(Contacts.userPublicationName);
-  // Determine if the subscription is ready
-  const ready = subscription.ready();
-  // Get the document
-  const doc = Contacts.collection.findOne(documentId);
-  return {
-    doc,
-    ready,
-  };
-})(EditContact);
+export default ProfessorReview;
