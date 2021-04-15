dcc4a28ae56886ef388db44ee7d984bca48034bd
sle417 <77819979+sle417@users.noreply.github.com>
2021-04-15 11:27:14 -1000

    fixed issue-6 branch complications
    changes to issue-6 did not update in master, so I redid the changes onto a new branch.

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,25 +1,7 @@
 import { Meteor } from 'meteor/meteor';
-import { Roles } from 'meteor/alanning:roles';
-import { Stuffs } from '../../api/stuff/Stuff';
+import { Profiles } from '../../api/profiles/Profiles';
 
 // User-level publication.
-// If logged in, then publish documents owned by this user. Otherwise publish nothing.
-Meteor.publish(Stuffs.userPublicationName, function () {
-  if (this.userId) {
-    const username = Meteor.users.findOne(this.userId).username;
-    return Stuffs.collection.find({ owner: username });
-  }
-  return this.ready();
-});
-
-// Admin-level publication.
-// If logged in and with admin role, then publish all documents from all users. Otherwise publish nothing.
-Meteor.publish(Stuffs.adminPublicationName, function () {
-  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
-    return Stuffs.collection.find();
-  }
-  return this.ready();
-});
 
 // alanning:roles publication
 // Recommended code to publish roles for each user.
@@ -29,3 +11,6 @@ Meteor.publish(null, function () {
   }
   return this.ready();
 });
+
+/** Define a publication to publish all profiles. */
+Meteor.publish(Profiles.userPublicationName, () => Profiles.collection.find());
---
+++ app/imports/ui/pages/Profiles.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Container, Loader, Card, Image, Rating, Header } from 'semantic-ui-react';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import { _ } from 'meteor/underscore';
+import { NavLink } from 'react-router-dom';
+import { Profiles } from '../../api/profiles/Profiles';
+
+/** Returns the Profile and associated Projects and Interests associated with the passed user email. */
+function getProfilesData(email) {
+  const data = Profiles.collection.findOne({ email });
+  return _.extend({ }, data);
+}
+
+/** Component for layout out a Profile Card. */
+const MakeCard = (props) => (
+  <Card color='green' centered className="profile-professors">
+    <Card.Content>
+      <Image as={NavLink} exact to="/john" src={props.profile.picture} />
+      <Card.Header>{props.profile.firstName} {props.profile.lastName}</Card.Header>
+      <Card.Meta>
+        <span className='date'>{props.profile.title}</span>
+      </Card.Meta>
+      <Card.Meta>
+        <Rating icon='star' defaultRating={4} maxRating={5} />
+      </Card.Meta>
+    </Card.Content>
+    <Card.Content extra>
+      <Header as='h5'>Courses</Header>
+      <Card.Description>
+        {props.profile.bio}
+      </Card.Description>
+    </Card.Content>
+  </Card>
+);
+
+MakeCard.propTypes = {
+  profile: PropTypes.object.isRequired,
+};
+
+/** Renders the Profile Collection as a set of Cards. */
+class ProfilesPage extends React.Component {
+
+  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
+  render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  /** Render the page once subscriptions have been received. */
+  renderPage() {
+    const emails = _.pluck(Profiles.collection.find().fetch(), 'email');
+    const profileData = emails.map(email => getProfilesData(email));
+    return (
+      <Container centered fluid id="profiles-page" className="professors-page">
+        <Card.Group>
+          {_.map(profileData, (profile, index) => <MakeCard key={index} profile={profile}/>)}
+        </Card.Group>
+      </Container>
+    );
+  }
+}
+
+ProfilesPage.propTypes = {
+  ready: PropTypes.bool.isRequired,
+};
+
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(() => {
+  // Ensure that minimongo is populated with all collections prior to running render().
+  const sub1 = Meteor.subscribe(Profiles.userPublicationName);
+  return {
+    ready: sub1.ready(),
+  };
+})(ProfilesPage);
