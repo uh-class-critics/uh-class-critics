fac9b96ed99be04858432826bf06e281c7be094d
John Suelen <jsuelen@hawaii.edu>
2021-04-15 10:52:14 -1000

    created footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
+import { Container, Grid, Image, List } from 'semantic-ui-react';
 
-/** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
-class Footer extends React.Component {
+export default class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
     return (
-      <footer>
-        <div style={divStyle} className="ui center aligned container">
-          <hr />
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822 <br />
-          <a href="http://ics-software-engineering.github.io/meteor-application-template-react">Template Home Page</a>
-        </div>
-      </footer>
+      <div className="footer">
+        <Container>
+          <Grid columns={3}>
+            <Grid.Column>
+              <Image src="/images/class-critics-logo.png" size="small"/>
+            </Grid.Column>
+            <Grid.Column>
+              <List>
+                <List.Item>About Us</List.Item>
+                <hr/>
+                <List.Item><a href="https://uh-class-critics.github.io/">GitHub Home Page</a></List.Item>
+                <List.Item><a href="https://github.com/uh-class-critics/uh-class-critics">Source Code</a></List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column>
+              <List>
+                <List.Item>Contact Us</List.Item>
+                <hr/>
+                <List.Item><a href="https://acatarinaoaraujo.github.io/">Anna Araujo</a>: acoa@hawaii.edu</List.Item>
+                <List.Item><a href="https://johnny-ho1.github.io/">Johnny Ho</a>: johnnyho@hawaii.edu</List.Item>
+                <List.Item><a href="https://zijunhuang-1.github.io/">Zi Jun Huang</a>: huangz8@hawaii.edu</List.Item>
+                <List.Item><a href="https://sle417.github.io/">Steven Le</a>: stevenle@hawaii.edu</List.Item>
+                <List.Item><a href="https://johnsuelen.github.io/">John Suelen</a>: jsuelen@hawaii.edu</List.Item>
+              </List>
+            </Grid.Column>
+          </Grid>
+        </Container>
+      </div>
     );
   }
 }
-
-export default Footer;
