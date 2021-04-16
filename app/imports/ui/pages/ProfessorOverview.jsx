4476d31423bcc6a4874d6339ea09a3e10bfdc0b9
John Suelen <jsuelen@hawaii.edu>
2021-04-15 23:26:16 -1000

    Update ProfessorOverview.jsx

--- app/imports/ui/pages/ProfessorOverview.jsx
+++ app/imports/ui/pages/ProfessorOverview.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Image, Card, Header, Rating, Container, Grid, Icon, Button } from 'semantic-ui-react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 class Landing extends React.Component {
   render() {
