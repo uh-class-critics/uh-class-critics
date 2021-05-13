5caf24b123244640477c898bfa48ac390449134c
sle417 <77819979+sle417@users.noreply.github.com>
2021-05-12 23:16:37 -1000

    Update ListProfessors.jsx

--- app/imports/ui/pages/ListProfessors.jsx
+++ app/imports/ui/pages/ListProfessors.jsx
@@ -34,18 +34,16 @@ class ListProfessors extends React.Component {
     const sorted = _.sortBy(allFirstNames, 'firstName');
 
     return (
-      <div className="list-professors-page">
-        <Container>
-          <Header as="h2" textAlign="center">Information & Computer Science Professors</Header>
-          <br/><br/>
-          <Input inverted type='text' size='large' placeholder='Search here...' icon='search' fluid
-            onChange={this.handleChange}/>
-          <br/><br/><br/><br/>
-          <Card.Group>
-            {sorted.map((professors, index) => <Professor key={index} professor={professors}/>)}
-          </Card.Group>
-        </Container>
-      </div>
+      <Container>
+        <Header as="h2" textAlign="center">Information & Computer Science Professors</Header>
+        <br/><br/>
+        <Input inverted type='text' size='large' placeholder='Search here...' icon='search' fluid
+          onChange={this.handleChange}/>
+        <br/><br/><br/><br/>
+        <Card.Group>
+          {sorted.map((professors, index) => <Professor key={index} professor={professors}/>)}
+        </Card.Group>
+      </Container>
     );
   }
 }
